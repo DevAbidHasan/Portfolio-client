015f4293cc9116b3a070644e1478e316cdae30a3
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-10-17 14:40:14 +0600

    first commit : project setup done with react, tailwind, and packages installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <p className='poppins text-5xl font-bold'>this is poppins</p>
+    <p className='ibm text-5xl font-bold'>this is ibm</p>
+    </>
+  )
+}
+
+export default App
